759dcab2807ab829a03a3913b242f1cd9f058f1f
github username <github email address>
2018-09-20 09:11:30 -0400

    different approach

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,17 +15,22 @@ class String
   end
 
   def count_sentences
+    # use regular expression to split on listed above
     splited_string = self.split(/[!?.]+/)
+    #return lenght or use size
     splited_string.length
 
+    # ******* MORE SOLUTION
 
-# string = self.squeeze(”.”)
-#  string = string.squeeze(”!”)
-# string.split(/[$.|?|!]/).count
-    # if self.length < 0
-    #   return 0
-    # end
-    # self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
-  # splits string into Array, collects those would-be sentences that actually contain word characters, removes false values, then counts the remaining booleans.
-  end
+      #self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+
+
+    # string = self.squeeze(”.”)
+    #  string = string.squeeze(”!”)
+    # string.split(/[$.|?|!]/).count
+
+        # self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
+      # splits string into Array, collects those would-be sentences that actually contain word characters, removes false values, then counts the remaining booleans.
+
+ end
 end
