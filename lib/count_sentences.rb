282e44b5c241b3d5e38a3d778e4bd0b1a0fed7f0
Cristina Murillo <murillocam96@gmail.com>
2018-09-20 20:10:16 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    array = self.split(" ")
+    count = 0 
+    array.each do |string|
+      if string.end_with?(".","?","!")
+        count+=1
+      end   
+    end
+    count 
   end
+
 end
